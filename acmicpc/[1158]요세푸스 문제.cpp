482fc9d081f9e1492af3b45ba5d4020874594d60
vecum0814 <52812351+vecum0814@users.noreply.github.com>
2021-02-03 23:39:09 +0900

    Create [1158]요세푸스 문제.cpp

---
+++ acmicpc/[1158]요세푸스 문제.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, K;
+    cin >> N >> K;
+    queue<int> q;
+    cout << '<';
+    for(int i = 1; i <= N; i++){
+        q.push(i);
+    }
+    for(int i = 0; i < N - 1; i++){
+        for(int j = 0; j < K - 1; j++){
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front() << ',' << " ";
+        q.pop();
+    }
+    cout << q.front() << '>' << '\n';
+    return 0;
+}
